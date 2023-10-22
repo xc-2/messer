75fd826e3d6ae5d6a1779bd45bbaffa15dd91b7e
Gerald <code@gera2ld.space>
2023-10-22 13:38:09 +0800

    feat: support backup for multiple entries

--- shell/restic-backup.sh
+++ shell/restic-backup.sh
@@ -3,21 +3,21 @@
 set -e
 
 RESTIC=${RESTIC:-restic}
-MESSER=$(dirname $0)
+RESTIC_DATA_PATH=${RESTIC_DATA_PATH:-.}
 
-if [ -z "$RESTIC_DATA_PATH" ]; then
-  echo 'RESTIC_DATA_PATH is required'
+if [ -z "$RESTIC_DATA_ROOT" ]; then
+  echo 'RESTIC_DATA_ROOT is required'
   exit 1
 fi
 
-echo Backing up $RESTIC_DATA_PATH to $RESTIC_REPOSITORY
-cd $RESTIC_DATA_PATH
+echo Backing up from $RESTIC_DATA_ROOT to $RESTIC_REPOSITORY: $RESTIC_DATA_PATH
+cd $RESTIC_DATA_ROOT
 
-added=$($RESTIC backup -qn $RESTIC_OPTS --json . | jq .data_added)
+added=$($RESTIC backup -qn $RESTIC_OPTS --json $RESTIC_DATA_PATH | jq .data_added)
 
 if [[ "$added" == "0" ]]; then
-  echo Repo is up to date: $RESTIC_DATA_PATH
+  echo Repo is up to date: $RESTIC_DATA_ROOT
   exit ${CODE_NO_CHANGE:-0}
 fi
 
-$RESTIC -v backup $RESTIC_OPTS .
+$RESTIC -v backup $RESTIC_OPTS $RESTIC_DATA_PATH
